e302417fd916b70013688e7b653f867cf3878fa7
garava <garava@altimetrik.com>
2024-12-08 23:21:17 +0530

    added SpringBootMongo add JWT authentication

--- src/main/java/com/springboot/devops/controllers/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.springboot.devops.controllers;
-
-public class UserController {
-}
---
+++ src/main/java/com/springboot/devops/security/LoginForm.java
@@ -0,0 +1,4 @@
+package com.springboot.devops.security;
+
+public record LoginForm(String username, String password) {
+}
